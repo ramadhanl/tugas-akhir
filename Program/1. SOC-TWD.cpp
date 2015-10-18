1c9a0b365b9b0cdc0678a624e1b7c336138619a7
Ramadhan Rosihadi Perdana <ramadhanrperdana@gmail.com>
2015-10-19 03:14:38 +0700

    Inisiasi tugas akhir

---
+++ Program/1. SOC-TWD.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+//Static overlapping clustering in the initial data set
+
+//Input : 
+//1. U = {x1, ... ,xN}
+//2. a
+//3. b
+//4. t
+
+//Output : 
+//1. C = {[clower[i],cupper[i]], ... ,[clower[N],cupper[N]]
+
+//Algorithm
+
+//Deklarasi variabel
+//1. R=0 : 
+//2. Neighbor(r[i])
+//3. r[i] elemen dari R
+//4. G
+
+//Hitung jarak matriks NxN (Distance(x,y))
+for (each row x[i] in [Distance(x,y)]) do
+	for (each column x[j] in [Distance(x,y))]) do
+		if(Distance(x,y)<= t) then
+			Neighbor(x[i]) = Neighbor(x[i]) union {x[j]}
+
+//Urutkan xi berdasarkan nilai dari |Neighbor(xi)| dalam urutan descending
+
+//Mencari titik representatif pada U
+new=0
+while([Distance(x,y) != []]){
+	new+=1;
+	Set the centroid of rnew be the objek x1 corresponding to the first
+}
+
